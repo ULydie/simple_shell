8c1ba718dc9606d9bd337c73f48c961193b8f0e3
ULydie <110610349+ULydie@users.noreply.github.com>
2024-02-26 13:04:41 +0200

    handle bultins

---
+++ handle_bultin.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+* handle_builtins - handles execution of builtins functions
+* @cmd:..
+* @lines:..
+*
+* Return:..
+*/
+int handle_builtins(char **cmd, char *lines)
+{
+	struct builtins builtins = {"env", "exit"};
+
+	if (_strcmp(*cmd, builtins.env) == 0)
+	{
+		print_environ();
+		return (1);
+	}
+	else if (_strcmp(*cmd, builtins.exit) == 0)
+	{
+		exit_cmd(cmd, lines);
+		return (1);
+	}
+	return (0);
+}
