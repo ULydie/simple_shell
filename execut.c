96ce8bcae668ce94b0674df43615c96afe9e53e2
ULydie <110610349+ULydie@users.noreply.github.com>
2024-02-26 12:49:50 +0200

    execution file

---
+++ execut.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * exec - executes commands entered by users
+ *@cp: cmd
+ *@cmd:..
+ * Return:..
+ */
+void exec(char *cp, char **cmd)
+{
+	pid_t child_pid;
+	int statu;
+	char **envir = environ;
+
+	child_pid = fork();
+	if (child_pid < 0)
+		perror(cp);
+	if (child_pid == 0)
+	{
+		execve(cp, cmd, env);
+		perror(cp);
+		free(cp);
+		freeBuffers(cmd);
+		exit(98);
+	}
+	else
+		wait(&statu);
+}
